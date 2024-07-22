f02782072e81cb07d7675d3f3837b86a0ae8566c
Monicah-Ngari <wakarimangari@gmail.com>
2024-07-22 09:12:54 +0300

    added alog

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Nav from "./Nav";
+import Logo from "../Images/Logo,jpeg";
 
 const Header = () => {
   return (
     <div className="Header">
       <Nav />
+      <header>
+        <img src={Logo} alt="Logo" style={{ width: "150px", height: "auto" }} />
+      </header>
 
       <h1>Monicah Ngari - Web Developer</h1>
     </div>
