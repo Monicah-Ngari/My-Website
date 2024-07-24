093d870a195369461f9e77e22f49565b0aace9eb
Monicah-Ngari <wakarimangari@gmail.com>
2024-07-24 21:39:54 +0300

    styled contact part

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -3,17 +3,25 @@ import "../styles.css";
 
 const Contact = () => {
   return (
-    <div className="Contact">
+    <div className="contact">
       <h3>Contact Me</h3>
       <form>
-        <label>Name:</label> <br></br>
-        <input />
-        <label> Email:</label>
-        <br></br>
-        <input />
-        <label> Message</label>
-        <br></br>
-        <input />
+        <label for="name">Name:</label>
+        <br />
+        <input type="text" id="name" name="name" />
+        <br />
+
+        <label for="email">Email:</label>
+        <br />
+        <input type="email" id="email" name="email" />
+        <br />
+
+        <label for="message">Message:</label>
+        <br />
+        <textarea id="message" name="message"></textarea>
+        <br />
+
+        <button type="submit">Submit</button>
       </form>
     </div>
   );
--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -8,32 +8,27 @@ import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 const Footer = () => {
   return (
     <div className="contact-links">
-      <p>
-        <a
-          href="https://www.linkedin.com/in/monicah-ngari-96459321b/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FontAwesomeIcon icon={faLinkedin} aria-hidden="true" size="1x" />{" "}
-          LinkedIn
-        </a>
-      </p>
-      <p>
-        <a
-          href="https://github.com/Monicah-Ngari"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FontAwesomeIcon icon={faGithub} aria-hidden="true" size="1x" />{" "}
-          GitHub
-        </a>
-      </p>
-      <p>
-        <a href="mailto:wakarimangari@gmail.com">
-          <FontAwesomeIcon icon={faEnvelope} aria-hidden="true" size="1x" />{" "}
-          Email Me
-        </a>
-      </p>
+      <a
+        href="https://www.linkedin.com/in/monicah-ngari-96459321b/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FontAwesomeIcon icon={faLinkedin} aria-hidden="true" size="1x" />{" "}
+        LinkedIn
+      </a>
+
+      <a
+        href="https://github.com/Monicah-Ngari"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FontAwesomeIcon icon={faGithub} aria-hidden="true" size="1x" /> GitHub
+      </a>
+
+      <a href="mailto:wakarimangari@gmail.com">
+        <FontAwesomeIcon icon={faEnvelope} aria-hidden="true" size="1x" /> Email
+        Me
+      </a>
     </div>
   );
 };
