167bc67cdb7d46241206d3541c8ae1e4ff2c7ea7
Monicah-Ngari <wakarimangari@gmail.com>
2024-07-26 05:58:05 +0300

    imported emailjs and implimented in contact component

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,27 +1,41 @@
-import React from "react";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 import "../styles.css";
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm("service_3nb42lj", "template_97c9pgh", form.current, {
+        publicKey: "0N0dgoV7nbdJNBKbn",
+      })
+      .then(
+        () => {
+          console.log("Message sent 👍");
+        },
+        (error) => {
+          console.log("FAILED...", error.text);
+        }
+      );
+  };
+
   return (
     <div className="contact">
-      <h3>Contact Me</h3>
-      <form>
-        <label for="name">Name:</label>
-        <br />
-        <input type="text" id="name" name="name" />
-        <br />
-
-        <label for="email">Email:</label>
-        <br />
-        <input type="email" id="email" name="email" />
-        <br />
-
-        <label for="message">Message:</label>
-        <br />
-        <textarea id="message" name="message"></textarea>
-        <br />
-
-        <button type="submit">Submit</button>
+      <form ref={form} onSubmit={sendEmail}>
+        <label className="name">Name</label>
+        <input type="text" name="user_name" />
+        <br></br>
+        <label className="email">Email</label>
+        <input type="email" name="user_email" />
+        <br></br>
+        <label className="message">Message</label>
+        <textarea name="message" />
+        <br></br>
+        <input type="submit" value="Send" />
+        <br></br>
       </form>
     </div>
   );
