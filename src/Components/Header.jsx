a0a1cfa6b14ceb35bb62a734398332ea89bf13a1
Monicah-Ngari <wakarimangari@gmail.com>
2025-02-21 20:57:56 +0300

    added new css for the headre

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,19 +1,13 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "../styles.css";
 import NavigationBar from "./Nav";
-import Logo from "../Images/logo.svg";
+import logo from "../Images/logo.svg";
 
 const Header = () => {
   return (
     <div className="Header">
       <header>
-        <img
-          src={Logo}
-          alt="Logo"
-          style={{ width: "130px", height: "auto" }}
-          className="MyLogo"
-        />
+        <img src={logo} alt="Logo" className="logo " />
         <NavigationBar />
       </header>
     </div>
