c2efb064320864c5d49ad437ef45ad3830c938d1
Monicah-Ngari <wakarimangari@gmail.com>
2025-02-21 20:49:01 +0300

    added .svg for my logo display

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,12 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "../styles.css";
 import NavigationBar from "./Nav";
-import Logo from "../Images/logo.jpeg";
-import Home from "./Home";
-import Projects from "./Projects";
-import Skills from "./Skills";
-import Education from "./Education";
-import Contact from "./Contact";
+import Logo from "../Images/logo.svg";
 
 const Header = () => {
   return (
