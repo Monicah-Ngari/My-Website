041ba991b777555bef7b79304aa03bbafe3e89de
Monicah-Ngari <wakarimangari@gmail.com>
2025-02-21 21:22:35 +0300

    fixed the backgroyund color

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import "../styles.css";
 import NavigationBar from "./Nav";
-import logo from "../Images/logo.svg";
+import logo from "../Images/logoo.jpeg";
 
 const Header = () => {
   return (
     <div className="Header">
       <header>
-        <img src={logo} alt="Logo" className="logo " />
+        <img src={logo} alt="Logo" className="logo" />
         <NavigationBar />
       </header>
     </div>
