ad60ebee133709f453e33896b91a1d4f19a68d27
Monicah-Ngari <wakarimangari@gmail.com>
2025-02-22 22:59:44 +0300

    added new compoent for my language

---
+++ src/Components/Languages.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import css from "../Images/css.jpeg";
+import html5 from "../Images.html5.jpeg";
+import js from "../Images.js.jpeg";
+import react from "../Images/react.jpeg";
+
+function Languages() {
+  return (
+    <div>
+      {" "}
+      <div>
+        {" "}
+        <h1>My Proficient Languages</h1>
+      </div>
+      <div>
+        <img src={html5} alt="HTML5 image" />
+        <img src={css} alt="Css image" />
+        <img src={js} alt="Java sript image" />
+        <img src={react} alt="React image" />
+      </div>
+    </div>
+  );
+}
+
+export default Languages;
