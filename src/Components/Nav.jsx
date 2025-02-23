4d1db050352ea663806c7880725682e8a9ec8b27
Monicah-Ngari <wakarimangari@gmail.com>
2025-02-23 10:42:51 +0300

    added my resume on my nav bar

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -12,7 +12,15 @@ const NavigationBar = () => {
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#about">About</Nav.Link>
             <Nav.Link href="#projects">Projects</Nav.Link>
+
             <Nav.Link href="#contact">Contact</Nav.Link>
+            <a
+              href="https://docs.google.com/document/d/1pAhs6MR1amxnCOF9thGCBEYj7D5CIjWo_NOnC2ZF9Zg/edit?usp=drive_link"
+              className="resume-button"
+              download="Monicah_Ngari_Resume.pdf"
+            >
+              <i className="fas fa-file-alt"></i> Download My Resume
+            </a>
           </Nav>
         </Navbar.Collapse>
       </Container>
