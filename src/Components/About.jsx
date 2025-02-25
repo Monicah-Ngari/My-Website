0017a0b1f725788fa7a38ba6d22994c2ccbccbcf
Monicah-Ngari <wakarimangari@gmail.com>
2025-02-25 21:00:24 +0300

    added some content on my about & chamged the font

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -9,20 +9,25 @@ const About = () => {
           Hi, I’m Monicah Ngari, a dedicated front-end developer with a passion
           for creating visually appealing, interactive, and user-friendly
           digital experiences. I specialize in building modern, responsive, and
-          high-performance web applications using HTML, CSS, JavaScript, React,
-          and React Native. With hands-on experience in API integration, state
-          management, Linux environments, and tools like Postman, I thrive in
-          developing applications that are not only functional but also
-          optimized for performance and accessibility. I enjoy solving complex
-          problems, refining UI/UX interactions, and ensuring seamless user
-          experiences across different platforms. Beyond coding, I stay actively
-          engaged in exploring emerging technologies, contributing to
-          open-source projects, and continuously enhancing my expertise in
-          modern web development. I am always eager to collaborate with
-          like-minded professionals, embrace new challenges, and push the
-          boundaries of what’s possible in tech. When I’m not immersed in code,
-          I enjoy tech blogging, participating in hackathons, and staying
-          updated on the latest industry trends to drive innovation.
+          high-performance web applications using{" "}
+          <strong>
+            {" "}
+            HTML, CSS,Bootstrap,Tailwind, JavaScript, React, GraphQL and React
+            Native
+          </strong>{" "}
+          . With hands-on experience in API integration, state management, Linux
+          environments, and tools like Postman, I thrive in developing
+          applications that are not only functional but also optimized for
+          performance and accessibility. I enjoy solving complex problems,
+          refining UI/UX interactions, and ensuring seamless user experiences
+          across different platforms. Beyond coding, I stay actively engaged in
+          exploring emerging technologies, contributing to open-source projects,
+          and continuously enhancing my expertise in modern web development. I
+          am always eager to collaborate with like-minded professionals, embrace
+          new challenges, and push the boundaries of what’s possible in tech.
+          When I’m not immersed in code, I enjoy tech blogging, participating in
+          hackathons, and staying updated on the latest industry trends to drive
+          innovation.
         </p>
       </section>
     </div>
